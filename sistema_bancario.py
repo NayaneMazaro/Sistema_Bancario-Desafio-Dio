b2b963954e668d6779a1fa335d075ad2fe813cf7
Nayane Mazaro <90645768+NayaneMazaro@users.noreply.github.com>
2024-10-13 14:22:24 -0300

    v0.1 sistema_bancario.py

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -1,79 +1,48 @@
-from datetime import datetime
-
 if __name__ == "__main__":
-
-    def menu():
-        menu = """
-        [d] -> Depositar
-        [s] -> Sacar
-        [e] -> Extrato
-        [q] -> Sair
-
-        -> """
-        return input(menu)
-
-    def depositar(saldo, valor, extrato, num_transacoes):
-        if num_transacoes < 10:
-            valor = float(input("Qual o valor do depósito?: "))
-            if valor > 0:
-                data_hora = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
-                saldo += valor 
-                extrato += f"{data_hora}\tDepósito:\tR$ {valor:.2f}\n"
-                num_transacoes += 1
-                print("Depósito realizado com sucesso!")
-            else:
-                print ("Operação falhou! Insira um valor válido.")
-        else:
-            print("Operação falhou! Limite de transações diárias atingido.")
-        
-        return saldo, extrato, num_transacoes
-
-    def sacar(saldo, valor, extrato, num_transacoes):
-        if num_transacoes < 10:
-            valor = int(input("Qual o valor do saque?: "))
-            if valor > 0:
-                if valor <= saldo and valor <= 500:
-                    data_hora = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
-                    saldo -= valor
-                    extrato += f"{data_hora}\tSaque:\t\tR$ {valor:.2f}\n"
-                    num_transacoes += 1
-                    print("Saque realizado com sucesso!")
-                elif valor > 500:
-                    print("Operação falhou! O valor do saque excede o limite.")
-                else:
-                    print("Operação falhou! Saldo insuficiente.")
-            else:
-                print ("Operação falhou! Insira um valor válido.")
-        else:
-            print("Operação falhou! Limite de transações diárias atingido.")
-
-        return saldo, extrato, num_transacoes
     
-    def exibir_extrato(saldo, extrato):
-        print("\n==================== EXTRATO ====================")
-        print("Não foram realizadas movimentações." if not extrato else extrato)
-        print(f"\n\t\t\tSaldo:\t\tR$ {saldo:.2f}")
-        print("==================================================")
+    saldo = 0
+    limite = 500
+    extrato = ""
+    contador = 1
 
-    def main():
-        saldo = 0
-        extrato = ""
-        num_transacoes = 0
+    menu = """
+    [d] -> Depositar
+    [s] -> Sacar
+    [e] -> Extrato
+    [q] -> Sair
 
-        while True:
-            opcao = menu()
-            valor = None
-            match opcao:
-                case "d":
-                    saldo, extrato, num_transacoes = depositar(saldo, valor, extrato, num_transacoes)
+    -> """
 
-                case "s":
-                    saldo, extrato, num_transacoes = sacar(saldo, valor, extrato, num_transacoes)
+    while True:
+        opcao = input(menu)
+        match opcao:
+            case "d":
+                valor = float(input("Qual o valor do deposito?: "))
+                if valor > 0:
+                    saldo += valor 
+                    extrato += f"Depósito: R$ {valor:.2f}\n"
+                else:
+                    print ("Operação falhou! Insira um valor válido.")
 
-                case "e":
-                    exibir_extrato(saldo, extrato)
+            case "s":
+                if contador <= 3:
+                    valor = int(input("Qual o valor do saque?: "))
+                    if valor <= saldo and valor <= 500:
+                        saldo -= valor
+                        contador += 1
+                        print("Saque realizado com sucesso!")
+                    elif valor > 500:
+                        print("Operação falhou! O valor do saque excede o limite.")
+                    else:
+                        print("Operação falhou! Saldo insuficiente.")
+                else:
+                    print("Operação falhou! Limite de saques atingido.")
 
-                case "q":
-                    break
-                
-main()
+            case "e":
+                print("\n================ EXTRATO ================")
+                print("Não foram realizadas movimentações." if not extrato else extrato)
+                print(f"\nSaldo: R$ {saldo:.2f}")
+                print("==========================================")
+
+            case "q":
+                break
